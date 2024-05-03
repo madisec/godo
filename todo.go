f7a0d40e44418bc3c160888e3fe440a9858debe6
madisec <madiisec@gmail.com>
2024-05-04 02:13:55 +0330

    Full commit for todo code

--- todo.go
+++ todo.go
@@ -3,9 +3,12 @@ package todo
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"time"
+
+	"github.com/alexeyco/simpletable"
 )
 
 type item struct {
@@ -24,26 +27,31 @@ func (t *Todos) Add(task string) {
 		CreatedAt:   time.Now(),
 		CompletedAt: time.Time{},
 	}
+
 	*t = append(*t, todo)
 }
 
-func (t *Todos) Complete(index int) {
+func (t *Todos) Complete(index int) error {
 	ls := *t
 	if index <= 0 || index > len(ls) {
-		return errors.New("Invalid index")
+		return errors.New("invalid index")
 	}
+
 	ls[index-1].CompletedAt = time.Now()
 	ls[index-1].Done = true
+
 	return nil
 }
 
 func (t *Todos) Delete(index int) error {
 	ls := *t
 	if index <= 0 || index > len(ls) {
-		return errors.New("Invalid index")
+		return errors.New("invalid index")
 	}
+
 	*t = append(ls[:index-1], ls[index:]...)
-	return nill
+
+	return nil
 }
 
 func (t *Todos) Load(filename string) error {
@@ -54,12 +62,77 @@ func (t *Todos) Load(filename string) error {
 		}
 		return err
 	}
+
 	if len(file) == 0 {
 		return err
 	}
 	err = json.Unmarshal(file, t)
-	if err != nill {
+	if err != nil {
 		return err
 	}
+
 	return nil
+}
+
+func (t *Todos) Store(filename string) error {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func (t *Todos) Print() {
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done?"},
+			{Align: simpletable.AlignRight, Text: "CreatedAt"},
+			{Align: simpletable.AlignRight, Text: "CompletedAt"},
+		},
+	}
+
+	var cells [][]*simpletable.Cell
+
+	for idx, item := range *t {
+		idx++
+		task := blue(item.Task)
+		done := blue("no")
+		if item.Done {
+			task = green(fmt.Sprintf("\u2705 %s", item.Task))
+			done = green("yes")
+		}
+		cells = append(cells, *&[]*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", idx)},
+			{Text: task},
+			{Text: done},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: item.CompletedAt.Format(time.RFC822)},
+		})
+	}
+
+	table.Body = &simpletable.Body{Cells: cells}
+
+	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+		{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("You have %d pending todos", t.CountPending()))},
+	}}
+
+	table.SetStyle(simpletable.StyleUnicode)
+
+	table.Println()
+}
+
+func (t *Todos) CountPending() int {
+	total := 0
+	for _, item := range *t {
+		if !item.Done {
+			total++
+		}
+	}
+
+	return total
 }
