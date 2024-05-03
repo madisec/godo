b16bae0435418ef49a79617023edc434c900d1cc
madisec <madiisec@gmail.com>
2024-05-04 00:30:50 +0330

    make functions for tool feature

---
+++ todo.go
@@ -0,0 +1,43 @@
+package todo
+
+import (
+	"errors"
+	"time"
+)
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item
+
+func (t *Todos) Add(task string) {
+	todo := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(index int) {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("Invalid index")
+	}
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+	return nil
+}
+
+func (t *Todos) Delete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("Invalid index")
+	}
+	*t = append(ls[:index-1], ls[index:]...)
+}
