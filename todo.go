2ac1cb332cab925b28890ddbfabebd8053e0699f
madisec <madiisec@gmail.com>
2024-05-04 00:42:33 +0330

    Set load file function

--- todo.go
+++ todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -40,4 +43,23 @@ func (t *Todos) Delete(index int) error {
 		return errors.New("Invalid index")
 	}
 	*t = append(ls[:index-1], ls[index:]...)
+	return nill
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	if len(file) == 0 {
+		return err
+	}
+	err = json.Unmarshal(file, t)
+	if err != nill {
+		return err
+	}
+	return nil
 }
